1e220d05fa7f87fa824f1bacb9af422cc275e596
Prithviraj Deshmane <prithvirajdeshmane@users.noreply.github.com>
2024-06-17 11:16:48 -0700

    Update questions.sql for Q2

--- SQL Deep Dive/Operator precendence/questions.sql
+++ SQL Deep Dive/Operator precendence/questions.sql
@@ -6,9 +6,9 @@
 * Select people either under 30 or over 50 with an income above 50000
 * Include people that are 50
 * that are from either Japan or Australia
-* My answer: select * from customers where (age < 30 or age >= 50) and (income > 50000) and (country = 'Japan' or country = 'Australia')
-* Provided answer: 
+* Ans: select * from customers where (age < 30 or age >= 50) and (income > 50000) and (country = 'Japan' or country = 'Australia')
 */
+select * from customers where (age < 30 or age >= 50) and (income > 50000) and (country = 'Japan' or country = 'Australia')
 
 /*
 * DB: Store
@@ -16,3 +16,10 @@
 * Question: 
 * What was our total sales in June of 2004 for orders over 100 dollars?
 */
+
+/* 
+* select sum(totalamount) from orders where (orderdate between '2004-06-01' and '2004-06-30') and totalamount > 100
+* select sum(totalamount) from orders where totalamount > 100 and (orderdate >= '2004-06-01' and orderdate <= '2004-06-30')
+*/
+
+
