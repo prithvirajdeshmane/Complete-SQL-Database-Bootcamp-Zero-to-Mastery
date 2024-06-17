9e5faa30ebbcfc2371768626a92f7d8e80a9b355
Prithviraj Deshmane <prithvirajdeshmane@users.noreply.github.com>
2024-06-17 11:09:59 -0700

    Update questions.sql for Q1

--- SQL Deep Dive/Operator precendence/questions.sql
+++ SQL Deep Dive/Operator precendence/questions.sql
@@ -6,6 +6,8 @@
 * Select people either under 30 or over 50 with an income above 50000
 * Include people that are 50
 * that are from either Japan or Australia
+* My answer: select * from customers where (age < 30 or age >= 50) and (income > 50000) and (country = 'Japan' or country = 'Australia')
+* Provided answer: 
 */
 
 /*
