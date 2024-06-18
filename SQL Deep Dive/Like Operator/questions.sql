0faa14da4533f4a1db137300e075d8055ec93760
Prithviraj Deshmane <prithvirajdeshmane@users.noreply.github.com>
2024-06-17 20:55:42 -0700

    Update questions.sql with answers

--- SQL Deep Dive/Like Operator/questions.sql
+++ SQL Deep Dive/Like Operator/questions.sql
@@ -5,7 +5,8 @@
 * Sample output: https://imgur.com/vXs4093
 * Use EXTRACT (YEAR FROM AGE(birth_date)) we will learn about this in later parts of the course
 */
-SELECT ..., EXTRACT (YEAR FROM AGE(birth_date)) as "age" FROM employees;
+-- SELECT ..., EXTRACT (YEAR FROM AGE(birth_date)) as "age" FROM employees;
+select *, EXTRACT (YEAR FROM AGE(birth_date)) as "Age" from employees where first_name ilike 'm%'
 
 
 /*
@@ -14,6 +15,7 @@ SELECT ..., EXTRACT (YEAR FROM AGE(birth_date)) as "age" FROM employees;
 * Question: How many people's name start with A and end with R?
 * Expected output: 1846
 */
+select count(emp_no) from employees where first_name ilike 'a%r'
 
                                                   
 /*
@@ -22,7 +24,7 @@ SELECT ..., EXTRACT (YEAR FROM AGE(birth_date)) as "age" FROM employees;
 * Question: How many people's zipcode have a 2 in it?.
 * Expected output: 4211 
 */
-
+SELECT count(customerid) from customers where zip::text like '%2%'
 
 
 /*
@@ -31,6 +33,7 @@ SELECT ..., EXTRACT (YEAR FROM AGE(birth_date)) as "age" FROM employees;
 * Question: How many people's zipcode start with 2 with the 3rd character being a 1.
 * Expected output: 109 
 */
+SELECT count(customerid) from customers where zip::text like '2_1%'
 
 
 /*
@@ -40,4 +43,4 @@ SELECT ..., EXTRACT (YEAR FROM AGE(birth_date)) as "age" FROM employees;
 * Replace null values with "No State"                                                  
 * Expected output: https://imgur.com/AVe6G4c
 */
-
+select coalesce(state, 'No State') as State from customers where phone::text like '302%'
