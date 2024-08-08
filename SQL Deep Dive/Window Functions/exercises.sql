d4e54912a10246891bdde5089d5cb629fc13ec61
Prithviraj Deshmane <prithvirajdeshmane@users.noreply.github.com>
2024-08-08 16:56:27 -0700

    Update exercises.sql with answers

--- SQL Deep Dive/Window Functions/exercises.sql
+++ SQL Deep Dive/Window Functions/exercises.sql
@@ -4,8 +4,13 @@
 *  Table: Country
 */
 
-SELECT *
-FROM country;
+SELECT distinct 
+    continent,
+    sum(population) over (
+        partition by continent 
+    ) as "Cumulative population"
+from country 
+order by continent
 
 /*
 *  To the previous query add on the ability to calculate the percentage of the world population
@@ -17,8 +22,34 @@ FROM country;
 *  Table: Country
 */
 
-SELECT *
-FROM country;
+SELECT 
+distinct
+    continent,
+    sum(population) over (
+        partition by continent 
+    ) as "Continent population",
+    sum(population) over() as "World population",
+    concat(
+        round(
+            (sum(population::bigint) over (partition by continent) / sum(population::bigint) over () ) * 100,
+            2
+        )
+        , '%') as "Percentage population"
+from country 
+order by continent;
+
+SELECT
+  DISTINCT continent,
+  SUM(population) OVER w1 as"continent population",
+  CONCAT( 
+      ROUND( 
+          ( 
+            SUM( population::float4 ) OVER w1 / 
+            SUM( population::float4 ) OVER() 
+          ) * 100    
+      ),'%' ) as "percentage of population"
+FROM country 
+WINDOW w1 AS( PARTITION BY continent );
 
 
 /*
@@ -28,5 +59,13 @@ FROM country;
 *  Table: Regions (Join + Window function)
 */
 
-SELECT *
-FROM regions AS r;
+SELECT distinct
+    r.id as "Region Id",
+    r.name as "Region name",
+    count(t.name) over (
+        PARTITION by r.name
+    ) as "Total towns"
+from towns as t 
+join departments as d on (t.department = d.code)
+join regions as r on (d.region = r.code)
+order by r.id;
